87f4d0db227d32c17114eee751813bad5edcf3be
Anders Holm <aholmer89@gmail.com>
2021-07-06 21:30:05 +0200

    ADD spring boot

---
+++ src/main/java/com/andersholm/modern_webshop/ModernWebshopApplication.java
@@ -0,0 +1,13 @@
+package com.andersholm.modern_webshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModernWebshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModernWebshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andersholm/modern_webshop/ModernWebshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andersholm.modern_webshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModernWebshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
